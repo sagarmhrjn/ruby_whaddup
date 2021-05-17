e8550ee441508bd97d665d5044ad9b3350addd2b
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-05-17 22:53:52 +0545

    enable flash message display

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,4 +16,11 @@ ActiveStorage.start()
 
 $(document).on('turbolinks:load', function(){
     $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function(){
+        $(this)
+        .closest('.message')
+        .transition('fade')
+        ;
+    })
+    ;
 })

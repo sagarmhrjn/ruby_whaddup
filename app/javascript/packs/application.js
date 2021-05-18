838fa95559156385b95c74abc11edf43551544d5
Sagar Maharjan <sagar.maharjan@palmmind.com>
2021-05-18 16:13:58 +0545

    Beautify input box, functionality and create custom scope

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,10 +18,22 @@ export function scroll_bottom(){
         $('#messages').scrollTop($('#messages')[0].scrollHeight)
     }
 }
+
+function submit_message(){
+    $('#message_body').on('keydown',function(e){
+        if(e.key != undefined){
+            if(e.key === "Enter"){
+                $('button').trigger("click");
+                e.target.value = "";
+            }
+        }
+    })
+}
 $(document).on('turbolinks:load', function(){
     $('.ui.dropdown').dropdown();
     $('.message .close').on('click', function(){
         $(this).closest('.message').transition('fade');
     });
+    submit_message();
     scroll_bottom();
 })
